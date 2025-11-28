9ea24c137774831a81acffe1dd6c39aeb0af66c0
Carlos Loría Rodríguez <carlosloriar5@gmail.com>
2025-11-27 22:34:17 -0800

    Finished exercise

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-$(function() {
-  // your code here
-})
+$(function () {
+
+  // ----------------------
+  // Exercise 1 - Accordion
+  // ----------------------
+
+
+  $(".accordion-header").on("click", function () {
+
+    if ($(this).next().is(":visible")) {
+      $(this).next().slideUp();
+      return;
+    }
+    $(".accordion-content").slideUp();
+
+    $(this).next().slideDown();
+  });
+
+
+  $(".todos button").on("click", function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        const list = $(".todos ul");
+        list.empty();
+
+        data.todos.forEach(todo => {
+          list.append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function () {
+        alert("Error loading todos :(");
+      }
+    });
+  });
+
+});
